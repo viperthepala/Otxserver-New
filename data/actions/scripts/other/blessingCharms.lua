dd5fe8ab0572e2ac44ba5038afeb570f624694ba
Matt Gomez <mattyx14@users.noreply.github.com>
2017-08-24 11:50:49 -0500

    check it first

--- data/actions/scripts/other/blessingCharms.lua
+++ data/actions/scripts/other/blessingCharms.lua
@@ -12,12 +12,12 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return true
 	end
 
-	if player:hasBlessing(useItem.blessId, 1) then
+	if player:hasBlessing(useItem.blessId) then
 		player:say('You already possess this blessing.', TALKTYPE_MONSTER_SAY)
 		return true
 	end
 
-	player:addBlessing(useItem.blessId)
+	player:addBlessing(useItem.blessId, 1)
 	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, useItem.text .. ' protects you.')
 	player:getPosition():sendMagicEffect(CONST_ME_LOSEENERGY)
 	item:remove(1)
