f728f9317fb007c69625ae355dfd7bb406e7db81
Marco Oliveira <marcomoa@hotmail.com>
2017-09-17 23:34:58 -0300

    Organizando raids

---
+++ data/raids/scripts/mad_mage.lua
@@ -0,0 +1,3 @@
+function onRaid()
+	local monster = Game.createMonster("Mad Mage", Position(33360, 31873, 9))
+end
---
+++ data/raids/scripts/weakened_shlorg.lua
@@ -0,0 +1,3 @@
+function onRaid()
+	local monster = Game.createMonster("Weakened Shlorg", Position(33164, 31716, 9))
+end
