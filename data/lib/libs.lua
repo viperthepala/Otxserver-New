b7bec7f45d04b80fa5c5decedbdfe6bcda9b1ab8
Marco Oliveira <marcomoa@hotmail.com>
2017-09-17 23:41:28 -0300

    Nova organização de libs

--- data/lib/libs.lua
+++ data/lib/libs.lua
@@ -1,10 +1,14 @@
--- Miscellaneous library
-dofile('data/lib/miscellaneous/miscellaneous.lua')
 -- Core API functions implemented in Lua
 dofile('data/lib/core/core.lua')
+
+-- Miscellaneous library
+dofile('data/lib/miscellaneous/miscellaneous.lua')
+
 -- Compatibility library for our old Lua API
 dofile('data/lib/compat/compat.lua')
-dofile('data/lib/rewardboss.lua')
-dofile('data/lib/modalwindow.lua')
-dofile('data/lib/lionrock.lua')
+
+-- Custom library
+dofile('data/lib/custom/rewardboss.lua')
+dofile('data/lib/custom/modalwindow.lua')
+dofile('data/lib/custom/lionrock.lua')
 dofile('data/lib/events/libs.lua')
