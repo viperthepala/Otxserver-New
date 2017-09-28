57dee0f24aa902f930feab9cd2299c6d44dcf673
Erick Nunes <erickzenho@live.com>
2017-09-28 01:40:50 -0300

    fix some debugs on client 10

--- data/modules/scripts/prey system/preysystem.lua
+++ data/modules/scripts/prey system/preysystem.lua
@@ -473,7 +473,7 @@ function Player.getBonusReroll(self)
 end
 
 function Player.getRerollPrice(self)
-	return (self:getLevel()/2) * 500
+	return (self:getLevel()/2) * 100
 end
 
 function Player.preyRerollList(self, column)
