45f3e0b6d71f91eee4ff9859f74239183d3cd0f5
Erick Nunes <erickzenho@live.com>
2017-10-17 23:57:29 -0300

    fixed paralyze rune

--- data/spells/scripts/support/paralyze_rune.lua
+++ data/spells/scripts/support/paralyze_rune.lua
@@ -3,7 +3,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
 
 local condition = Condition(CONDITION_PARALYZE)
 condition:setParameter(CONDITION_PARAM_TICKS, 20000)
-condition:setFormula(-1, 80, -1, 80)
+condition:setFormula(-1, 1, -1,1)
 combat:setCondition(condition)
 
 function onCastSpell(creature, variant, isHotkey)
